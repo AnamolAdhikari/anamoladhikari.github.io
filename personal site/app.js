0af2fede4f176271afdda0fbbf428b35fc4535b6
Anamol Adhikari <anamoladhikari1998@gmail.com>
2022-04-01 21:44:05 -0600

    New Update

--- personal site/app.js
+++ personal site/app.js
@@ -4,3 +4,15 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
+var i = 0;
+var txt =
+  " GameSite is the simple to-involve skin commercial center for CSGO things that permits you to trade CSGO skins for genuine cash.";
+var speed = 35;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
